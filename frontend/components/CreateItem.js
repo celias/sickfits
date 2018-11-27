77a0996a2a1797f1ea28f94e3bdb6cb25061ed9b
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-11-27 17:18:13 -0600

    Added Cloudinary and ability to upload and save images

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -27,16 +27,13 @@ const CREATE_ITEM_MUTATION = gql`
 `;
 
 
-
-
-
 class CreateItem extends Component {
   state = {
-    title: 'cool shoes',
-    description: 'These are shoez',
-    image: 'shoez.jpg',
-    largeImage: 'shoez.jpg',
-    price: 8500,
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0,
   };
 
   handleChange = (e) => {
@@ -45,6 +42,24 @@ class CreateItem extends Component {
       value;
     this.setState({ [name]: val })
   }
+
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const response = await fetch('https://api.cloudinary.com/v1_1/sickfitss/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+    const file = await response.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -66,9 +81,23 @@ class CreateItem extends Component {
 
             <ErrorMessage error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img
+                  src={this.state.image} alt="upload preview" /> ? <img
+                    src={this.state.image} alt="upload preview" width='200px' /> : ''}
+              </label>
               <label htmlFor="title">
                 Title
-            <input
+                <input
                   type="text"
                   id="title"
                   name="title"
