90764205e3d027cbac1f13bfcd2f83b2060a9391
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-11-27 13:38:21 -0600

    Create Item input fields local state

---
+++ frontend/components/CreateItem.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql';
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+
+class CreateItem extends Component {
+  state = {
+    title: 'cool shoes',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0,
+  };
+
+  handleChange = (e) => {
+    const { name, type, value } = e.target;
+    const val = type === 'number' ? parseFloat(value) :
+      value;
+    this.setState({ [name]: val })
+  }
+  render() {
+    return (
+      <Form>
+        <fieldset>
+          <label htmlFor="title">
+            Title
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="price">
+            Price
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="description">
+            Description
+            <textarea
+              type="text"
+              id="description"
+              name="description"
+              placeholder="Description"
+              required
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </label>
+        </fieldset>
+      </Form>
+    )
+  }
+}
+export default CreateItem;
