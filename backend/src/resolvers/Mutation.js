7bc859138c24d4e50389c3d4d9fbdd9095470cb0
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-12-19 17:47:33 -0600

    User saving to db

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const bcyrpt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 // Pulling Data
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -6,8 +8,6 @@ const Mutations = {
         ...args
       },
     }, info);
-
-    console.log(item)
     return item;
   },
   async updateItem(parent, args, ctx, info) {
@@ -31,6 +31,34 @@ const Mutations = {
 
     // Delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // toLowerCase to ensure that the email works
+    args.email = args.email.toLowerCase();
+    // Hash the user's pw
+    const password = await bcyrpt.hash(args.password, 10)
+    // Create the user in the db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    // Create JWT token
+    // APP_SECRET comes from the variables.env file
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1yr long cookie
+    });
+    // Finallyyyy we return the user to the browser
+    return user;
   },
 };
 
