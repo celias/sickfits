8c8752bb49b668c2370c524b3176be62c0f9bdd4
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-12-28 19:16:12 -0600

    Backend password reset functionality

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,13 @@
-const bcyrpt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
+
+const cookieSettings = {
+  httpOnly: true,
+  maxAge: 1000 * 60 * 60 * 24 * 365,
+}
+
 // Pulling Data
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -38,7 +46,7 @@ const Mutations = {
     // toLowerCase to ensure that the email works
     args.email = args.email.toLowerCase();
     // Hash the user's pw
-    const password = await bcyrpt.hash(args.password, 10)
+    const password = await bcrypt.hash(args.password, 10)
     // Create the user in the db
     const user = await ctx.db.mutation.createUser(
       {
@@ -51,10 +59,7 @@ const Mutations = {
       info
     );
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365,
-    });
+    ctx.response.cookie('token', token, cookieSettings);
     return user;
   },
   // args is destructured as { email, password }
@@ -66,23 +71,75 @@ const Mutations = {
       throw new Error(`No such user found for email ${email}`);
     }
     // 2. Check if their PW is correct
-    const valid = await bcyrpt.compare(password, user.password);
+    const valid = await bcrypt.compare(password, user.password);
     if (!valid) {
       throw new Error('Invalid Password!');
     }
     // 3. Generate JWT Token
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365,
-    });
+    ctx.response.cookie('token', token, cookieSettings);
     return user;
   },
 
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token');
     return { message: 'Successfully signed out!' }
-  }
+  },
+
+  async requestReset(parent, args, ctx, info) {
+    // 1. Check if this is real user
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${args.email}`);
+    }
+    // 2. Set a reset token and expiry on that user
+    const randomBytesPromisifed = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisifed(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000; // 1 hr from now
+    const response = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry },
+    });
+    console.log(response);
+    return { message: 'BYYYYEEE' };
+    // 3. Email the reset token!
+  },
+  async resetPassword(parent, args, ctx, info) {
+    // 1. Check if the passwords match
+    if (args.password !== args.confirmPassword) {
+      throw new Error('Unmatched Passwords!')
+    };
+    // 2. Check if it's a legit reset token
+    // 3. Check if it's expired
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+      }
+    });
+    if (!user) {
+      throw new Error('This token is either valid or expired!')
+    }
+    // 4. Hash their new password
+    const password = await bcrypt.hash(args.password, 10);
+    // 5. Save the new password to the user and remove old resetToken fields
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+    // 6. Generate JWT
+    const token = jwt.sign({ userId: updatedUser.id },
+      process.env.APP_SECRET);
+    // 7. Set JWT cookie
+    ctx.response.cookie('token', token, cookieSettings);
+    // 8. Return the new user
+    return updatedUser;
+  },
+
 };
 
 module.exports = Mutations;
