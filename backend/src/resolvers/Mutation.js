0bd8dbd937ca26e7f464062fd5d389d71f10584f
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-12-31 22:21:20 -0600

    add to cart functionality is saving to the db

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -194,7 +194,43 @@ const Mutations = {
         id: args.userId,
       },
     }, info)
-  }
+  },
+  async addToCart(parent, args, ctx, info) {
+    // 1. Make sure they are signed in
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('You are not signed in!');
+    };
+    // 2. Query the users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems(
+      {
+        where: {
+          user: { id: userId },
+          item: { id: args.id }
+        }
+      });
+    // 3. Check if the item is already in their cart, and increment by +1 if it is
+    if (existingCartItem) {
+      console.log('Item already in cart')
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 },
+        },
+        info);
+    };
+    // 4. If its not, create a fresh cartItem for user
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: { id: userId },
+        },
+        item: {
+          connect: { id: args.id },
+        },
+      },
+    }, info);
+  },
 };
 
 module.exports = Mutations;
