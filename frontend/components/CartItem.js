be0f6feaafcb1903450257cf89b4461fb63b909f
Gabriella DiVentura <gabrielladiv@gmail.com>
2019-01-01 19:56:31 -0600

    Cart Feature

---
+++ frontend/components/CartItem.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import formatMoney from '../lib/formatMoney';
+import styled from 'styled-components';
+import PropTypes from 'prop-types'
+
+
+const CartItemStyles = styled.li`
+padding: 1rem 0;
+border-bottom: 1px solid ${props => props.theme.lightgrey};
+display: grid;
+align-items: center;
+grid-template-columns: auto 1fr auto;
+img {
+  margin-right: 10px;
+}
+h3, p {
+  margin: 0;
+}
+`;
+
+const CartItem = ({ cartItem }) => (
+  <CartItemStyles>
+    <div className="cart-item-details">
+      <img width="100px" src={cartItem.item.image} alt={cartItem.item.title} />
+      <h3>{cartItem.item.title}</h3>
+      <p>
+        {formatMoney(cartItem.item.price * cartItem.quantity)}
+        {' - '}
+        <em>{cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each</em>
+      </p>
+    </div>
+  </CartItemStyles>
+);
+
+CartItem.propTypes = {
+  cartItem: PropTypes.object.isRequired,
+};
+
+export default CartItem;
