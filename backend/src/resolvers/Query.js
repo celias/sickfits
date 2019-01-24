dbff67ac1e47a8f037deb232c2894b3882392539
Gabriella DiVentura <gabrielladiv@gmail.com>
2019-01-24 14:46:18 -0600

    Order Page complete

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -25,6 +25,25 @@ const Query = {
 
     // 3. If the user has permissions, query all of the users
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order({
+      where: { id: args.id },
+    }, info);
+    // 3. Check if they have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder =
+      ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error('You do not have adequate permissions for this order.');
+    }
+    // 4. Return the order
+    return order;
   }
 };
 
