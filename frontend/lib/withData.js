3a5fbcba3e2b4f9c43f85e806131ce6e685c3d1c
Gabriella DiVentura <gabrielladiv@gmail.com>
2019-02-04 17:37:44 -0600

    CORS

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -9,6 +9,7 @@ function createClient({ headers }) {
     request: operation => {
       operation.setContext({
         //
+        //
         // fetchOptions: {
         //   credentials: 'include',
         // },
